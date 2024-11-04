81a2b7ac413b9906dffc77950f3507a66fd8a5f3
Ariel Bendahan <116383489+arielbendahan@users.noreply.github.com>
2024-11-04 17:14:30 -0500

    Update config.ts

--- app/config.ts
+++ app/config.ts
@@ -1,7 +1,7 @@
 export const metaData = {
   baseUrl: "https://nextfolio-template.vercel.app/",
-  title: "Nextfolio",
-  name: "Sirius",
+  title: "Ariel Bendahan's Portfolio",
+  name: "Ariel Bendahan",
   ogImage: "/opengraph-image.png",
   description:
     "A clean, fast, and lightweight portfolio template built with Next.js, Vercel, and Tailwind CSS for optimal performance.",
@@ -9,8 +9,8 @@ export const metaData = {
 
 export const socialLinks = {
   twitter: "https://x.com/1tssirius",
-  github: "https://github.com/1msirius/Nextfolio",
+  github: "https://github.com/arielbendahan",
   instagram: "https://www.instagram.com/",
-  linkedin: "https://www.linkedin.com/",
-  email: "mailto:example@gmail.com",
+  linkedin: "https://www.linkedin.com/in/ariel-bendahan-a481b02a3/",
+  email: "mailto:arielbendahan01@gmail.com",
 };
