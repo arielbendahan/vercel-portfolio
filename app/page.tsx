ed45fcb77e524e40d34752bbb27999332bc37d3f
Ariel Bendahan <116383489+arielbendahan@users.noreply.github.com>
2024-11-23 22:54:12 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export default function Page() {
           alt="Profile photo"
           className="rounded-full block lg:mt-5 mt-0 lg:mb-5 mb-10 mx-auto sm:float-right sm:ml-5 sm:mb-5"
           unoptimized
-          width={160}
+          width={200}
           height={160}
           priority
         />
