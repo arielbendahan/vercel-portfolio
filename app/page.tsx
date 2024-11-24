b4b14dba9e44da3e17bb584a443d5039c209fceb
Ariel Bendahan <116383489+arielbendahan@users.noreply.github.com>
2024-11-23 22:49:33 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Page() {
   return (
     <section>
         <Image
-          src="/profile.png"
+          src="/profile.jpg"
           alt="Profile photo"
           className="rounded-full block lg:mt-5 mt-0 lg:mb-5 mb-10 mx-auto sm:float-right sm:ml-5 sm:mb-5"
           unoptimized
