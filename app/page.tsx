cb19882557738afc19c1872d0ea9bcc647b02514
Ariel Bendahan <116383489+arielbendahan@users.noreply.github.com>
2025-01-14 12:05:21 -0500

    Commented out pfp

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import { socialLinks } from "./config";
 export default function Page() {
   return (
     <section>
-        <Image
+{/*         <Image
           src="/profile.jpg"
           alt="Profile photo"
           className="rounded-full block lg:mt-5 mt-0 lg:mb-5 mb-10 mx-auto sm:float-right sm:ml-5 sm:mb-5"
@@ -12,7 +12,7 @@ export default function Page() {
           width={200}
           height={160}
           priority
-        />
+        /> */}
 
       <h1 className="mb-8 text-2xl font-medium tracking-tight">
         Welcome to my Portfolio!
