2d1f60f4e3b18d0ac35dacd63034175f513e2b1d
Ariel Bendahan <116383489+arielbendahan@users.noreply.github.com>
2025-01-27 21:30:53 -0500

    Added new project

--- app/projects/project-data.tsx
+++ app/projects/project-data.tsx
@@ -20,4 +20,11 @@ export const projects: Project[] = [
       "A website that features an online learning game designed for children. Built primarily using PHP for the game’s code, SQL for managing a user’s score history and uses CSS for the website’s styling.",
     url: "https://github.com/zikail/PrjPhp-FENTGames",
   },
+  {
+    title: "Property Rental Management",
+    year: 2024,
+    description:
+      "A web application that allows various user types to manage apartments, buildings and tenants. Built with C# and ASP.NET MVC for the main code and database relationships with the code, SQL for the database and HTML+CSS for the page styling",
+    url: "https://github.com/arielbendahan/Property-Rental-Management",
+  },
 ];
